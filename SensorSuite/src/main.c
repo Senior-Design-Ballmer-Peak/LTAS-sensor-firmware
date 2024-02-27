0c1aa615c892501ed51b464e4523b3db30f83417
Tristan Pawlenty <tristan-pawlenty@uiowa.edu>
2024-02-27 00:07:20 -0600

    Normal mode read task

--- SensorSuite/src/main.c
+++ SensorSuite/src/main.c
@@ -110,6 +110,7 @@ void BME280_delay_msek(u32 msek)
 	vTaskDelay(msek/portTICK_PERIOD_MS);
 }
 
+
 static esp_err_t
 icm20948_configure(icm20948_acce_fs_t acce_fs, icm20948_gyro_fs_t gyro_fs)
 {
@@ -240,6 +241,54 @@ void tx_task(void *pvParameter)
 	// never reach here
 	vTaskDelete( NULL );
 }
+
+void task_bme280_normal_mode(void *ignore)
+{
+	struct bme280_t bme280 = {
+		.bus_write = BME280_I2C_bus_write,
+		.bus_read = BME280_I2C_bus_read,
+		.dev_addr = BME280_I2C_ADDRESS2,
+		.delay_msec = BME280_delay_msek
+	};
+
+	s32 com_rslt;
+	s32 v_uncomp_pressure_s32;
+	s32 v_uncomp_temperature_s32;
+	s32 v_uncomp_humidity_s32;
+
+	com_rslt = bme280_init(&bme280);
+
+	com_rslt += bme280_set_oversamp_pressure(BME280_OVERSAMP_16X);
+	com_rslt += bme280_set_oversamp_temperature(BME280_OVERSAMP_2X);
+	com_rslt += bme280_set_oversamp_humidity(BME280_OVERSAMP_1X);
+
+	com_rslt += bme280_set_standby_durn(BME280_STANDBY_TIME_1_MS);
+	com_rslt += bme280_set_filter(BME280_FILTER_COEFF_16);
+
+	com_rslt += bme280_set_power_mode(BME280_NORMAL_MODE);
+	if (com_rslt == SUCCESS) {
+		while(true) {
+			vTaskDelay(40/portTICK_PERIOD_MS);
+
+			com_rslt = bme280_read_uncomp_pressure_temperature_humidity(
+				&v_uncomp_pressure_s32, &v_uncomp_temperature_s32, &v_uncomp_humidity_s32);
+
+			if (com_rslt == SUCCESS) {
+				ESP_LOGI(TAG_BME280, "%.2f degC / %.3f hPa / %.3f %%",
+					bme280_compensate_temperature_double(v_uncomp_temperature_s32),
+					bme280_compensate_pressure_double(v_uncomp_pressure_s32)/100, // Pa -> hPa
+					bme280_compensate_humidity_double(v_uncomp_humidity_s32));
+			} else {
+				ESP_LOGE(TAG_BME280, "measure error. code: %d", com_rslt);
+			}
+		}
+	} else {
+		ESP_LOGE(TAG_BME280, "init or setting error. code: %d", com_rslt);
+	}
+
+	vTaskDelete(NULL);
+}
+
 
 void
 app_main(void)
